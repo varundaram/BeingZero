78b98b1ff1182bd143a5212f25b0194eefd73273
varun sai <varundaram@gmail.com>
2019-12-19 12:55:50 +0530

    new website

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ const port =process.env.PORT || 3000
 app.use(express.static('public'))
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/public/html/index1.html'))
-
+app.get('/index2.html', (req, res) => res.sendFile(__dirname + '/public/html/index2.html'))
+app.get('/index3.html', (req, res) => res.sendFile(__dirname + '/public/html/index3.html'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
